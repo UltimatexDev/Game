23096544fb1e5d1be9e9095de33b3bbae99eab8e
Ultimate <64736890+UltimatexDev@users.noreply.github.com>
2021-06-17 06:27:49 +0000

    Added a function to call the games upon the users choice.

--- main.lua
+++ main.lua
@@ -4,14 +4,14 @@ function wait(second, millisecond)
 	end
 end
 
-function mathGame(input1, input2, name)
+function mathGame(input1, input2)
 	local a = input1
 	local b = input2
 	local result = 10 * 10
 	if a * b == result then
-		print("Good job "..name.." you won!")
+		print("Good job you won!")
 	else
-		print(name.." Go learn some math...")
+		print("Go learn some math...")
 	end
 end
 
@@ -19,10 +19,28 @@ function counterGame(input)
 	local names = {"Alex", "Djon", "Darth Vader", "God", "Jesus"}
 	local randName = math.random(1, #names)
 	print(names[randName])
-	if input == randName then
+	if input == names[randName] then
 		print("You won!")
 	else
 		print("You lose.")
+	end
+end
+
+function startGame(game)
+	if game == "MathGame" then
+		print("Fine you chose math!")
+		wait(1)
+		print("What do you need to multiply to get 100?")
+		wait(1)
+		print("input the first number.")
+		local input1 = io.read()
+		print("input the second number.")
+		local input2 = io.read()
+		mathGame(input1, input2)
+	elseif game == "NameGame" then
+		print("Fine you chose the game where you need to get the right name!\n please input the name you think that got generated!")
+		local inputx = io.read()
+		counterGame(inputx)
 	end
 end
 
@@ -39,19 +57,9 @@ if choice == "y" or choice == "yes" then
 	local input1 = io.read()
 
 	if input1 == "1" then
-		print("Fine you chose math!")
-		wait(1)
-		print("What do you need to multiply to get 100?")
-		wait(1)
-		print("input the first number.")
-		local input1 = io.read()
-		print("input the second number.")
-		local input2 = io.read()
-		mathGame(input1, input2, name)
+		startGame("MathGame")
 	elseif input1 == "2" then
-		print("Fine you chose the game where you need to get the right name!\n please input the name you think that got generated!")
-    local inputx = io.read()
-		counterGame(inputx)
+		startGame("NameGame")
 	end
 elseif choice == "n" or choice == "no" then
 	print("Okay... then tell me what your name is: ")
@@ -59,15 +67,18 @@ elseif choice == "n" or choice == "no" then
 	print("Your name is: "..name.." right...?(y = yes n = no)")
 	local choice = io.read()
 	if choice == "y" or choice == "yes" then
-		print("Welcome "..name.."!\n lets play some math..")
+		print("Welcome "..name.."!\n what do you want to play?\n input the number of the game you want to play!")
 		wait(1)
-		print("What do you need to multiply to get 100?")
-		wait(1)
-		print("input the first number.")
+		print("1: Math Game")
+		wait(0.5)
+		print("2: Counter Game")
 		local input1 = io.read()
-		print("input the second number.")
-		local input2 = io.read()
-		mathGame(input1, input2, name)
+
+		if input1 == "1" then
+			startGame("MathGame")
+		elseif input1 == "2" then
+			startGame("NameGame")
+		end
 	elseif choice == "n" or choice == "no" then
 		wait(5)
 		print("THEN WHAT THE HELL IS YOUR NAME?!")
